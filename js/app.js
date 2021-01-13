0a9f144c2722f55635862e150be2f2730981200d
Stephen Webber <offgridauthor@gmail.com>
2021-01-12 17:00:11 -0800

    feature 2

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 'use strict';
 const creaturesArray = [];
+const keywordArray = [];
 
-function HornedCreature(name, url, alt, keyword, horns){
+function HornedCreature(name, url, alt, keyword, horns) {
   this.name = name;
   this.src = url;
   this.alt = alt;
@@ -10,17 +11,18 @@ function HornedCreature(name, url, alt, keyword, horns){
   creaturesArray.push(this);
 }
 
-HornedCreature.prototype.display = function(){
+HornedCreature.prototype.display = function () {
   const $templateCopy = $('section:first-child').clone();
   const $h2 = $templateCopy.find('h2');
   $h2.text(this.name);
   const $img = $templateCopy.find('img');
   $img.attr('src', this.src);
   $img.attr('alt', this.alt);
+  $img.attr('class', this.keyword);
   const $p = $templateCopy.find('p');
   $p.text(this.alt);
   $('main').append($templateCopy);
-}
+};
 
 $.ajax('/data/page-1.json').then(hornedList => {
   console.log(hornedList);
@@ -30,4 +32,40 @@ $.ajax('/data/page-1.json').then(hornedList => {
   creaturesArray.forEach(horned => {
     horned.display();
   });
+
+  creaturesArray.forEach((object, index, array) => {
+    let isThere = false;
+    keywordArray.forEach((keyword2, index, array) => {
+      let x = keywordArray[index];
+      if (x === object.keyword) {
+        isThere = true;
+      }
+    });
+    if (isThere === false) {
+      const $optionClone = $('option:first-child').clone();
+      $optionClone.attr('value', `${object.keyword}`);
+      // $optionClone.attr('value', 'narwhal');
+      $optionClone.text(`${object.keyword}`);
+      // $optionClone.text('SOMETHING');
+      $('select').append($optionClone);
+      keywordArray.push(object.keyword);
+
+    }
+  });
+  console.log(keywordArray);
+
 });
+
+$('select').on('change', (event) => {
+  console.log(event);
+  let target = event.target;
+  $('section').hide();
+// currently only checking descriptions; need to figure out how to check class/title
+  $(`section:contains(${target.value})`).show();
+
+
+});
+
+
+
+
